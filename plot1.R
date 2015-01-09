556b1a91fab0a2fb02d121a4806e5204a0865fc0
mtylka <matt.tylka@gmail.com>
2015-01-09 13:08:36 -0500

    Course Project 1
    Course Project 1 PNG and R Files

---
+++ plot1.R
@@ -0,0 +1,6 @@
+dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+datQ <- subset(dat, Date=="1/2/2007" | Date=="2/2/2007")
+datQ$Global_active_power <- as.numeric(as.character(datQ$Global_active_power))
+png("plot1.png")
+hist(datQ$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+datQ <- subset(dat, Date=="1/2/2007" | Date=="2/2/2007")
+datQ$Global_active_power <- as.numeric(as.character(datQ$Global_active_power))
+datQ$DateTime <- as.POSIXct(paste(datQ$Date, datQ$Time), format="%d/%m/%Y %H:%M:%S")
+png("plot2.png")
+plot(datQ$DateTime, datQ$Global_active_power, type="l", ylab="Global Active Power(kilowatts)", xlab="")
+dev.off()
